6b7d611867bcfb9455dd2c499d09ea5528beb2c4
Serge Zenevich <zenevichsa@gmail.com>
2014-08-21 17:23:55 +0300

    no message

--- toolset/src/main/java/com/softteco/toolset/files/FileUploadServlet.java
+++ toolset/src/main/java/com/softteco/toolset/files/FileUploadServlet.java
@@ -1,10 +1,7 @@
 package com.softteco.toolset.files;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 import java.util.UUID;
 import javax.servlet.ServletException;
@@ -43,36 +40,26 @@ public abstract class FileUploadServlet extends HttpServlet {
 
     @Override
     protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FILE UPLOAD!!!");
+        
         String fileName = null;
         try {
             final FileItemFactory fileItemFactory = new DiskFileItemFactory(1000000, new File(getFolder()));
             final List<FileItem> items = new ServletFileUpload(fileItemFactory).parseRequest(req);
+            System.out.println("received POST with " + items.size() + " items.");
 
             for (FileItem item : items) {
                 if (item.isFormField()) {
                     continue;
                 }
 
-                final InputStream in = item.getInputStream();
-                final File file = getFile(fileName = getFileName(item));
-                final OutputStream out = new FileOutputStream(file);
-                final byte[] buffer = new byte[1024];
-                while (true) {
-                    int count = in.read(buffer);
-                    try {
-                        if (count < 1024) {
-                            break;
-                        }
-                    } finally {
-                        if (count > 0) {
-                            out.write(buffer, 0, count);
-                        }
-                    }
+                try {
+                    final File file = getFile(fileName = getFileName(item));
+                    item.write(file);
+                    fileUploaded(item, file, req);
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
                 }
-                out.flush();
-                out.close();
-                
-                fileUploaded(item, file, req);
             }
         } catch (FileUploadException e) {
             throw new ServletException(e);
@@ -82,9 +69,8 @@ public abstract class FileUploadServlet extends HttpServlet {
             resp.sendRedirect(req.getContextPath() + getRedirect() + "?uuid=" + fileName);
         }
     }
-    
+
     protected void fileUploaded(final FileItem item, final File file, final HttpServletRequest request) {
-        
     }
 
     protected abstract String getRedirect();

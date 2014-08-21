bd602dcc1d31503657049cd459ea3aa8497f3549
Serge Zenevich <zenevichsa@gmail.com>
2014-08-21 11:22:59 +0300

    no message

--- toolset/src/main/java/com/softteco/toolset/bl/AutofillAbstractAssembler.java
+++ toolset/src/main/java/com/softteco/toolset/bl/AutofillAbstractAssembler.java
@@ -26,7 +26,7 @@ public abstract class AutofillAbstractAssembler<E, D> extends AbstractAssembler<
         final Class entityClass = entity.getClass();
         for (Field each : getDtoClass().getFields()) {
             try {
-                Method method = null;
+                final Method method;
                 if (each.getType().equals(boolean.class)) {
                     method = entityClass.getMethod("is" + each.getName().substring(0, 1).toUpperCase() + each.getName().substring(1));
                 } else {
@@ -49,19 +49,18 @@ public abstract class AutofillAbstractAssembler<E, D> extends AbstractAssembler<
     public void disassemble(E entity, D dto) {
         final Class entityClass = entity.getClass();
         for (Field each : getDtoClass().getFields()) {
-            Method method = null;
             try {
-                method = entityClass.getMethod("set" + each.getName().substring(0, 1).toUpperCase() + each.getName().substring(1), each.getType());
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace(System.out);
-            }
+                final Method method = entityClass.getMethod("set" + each.getName().substring(0, 1).toUpperCase() + each.getName().substring(1), each.getType());
 
-            try {
-                method.invoke(entity, each.get(dto));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace(System.out);
-            } catch (InvocationTargetException e) {
-                e.printStackTrace(System.out);
+                try {
+                    method.invoke(entity, each.get(dto));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace(System.out);
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace(System.out);
+                }
+            } catch (NoSuchMethodException e) {
+                System.out.println("Skipped field " + each.getName());
             }
         }
     }

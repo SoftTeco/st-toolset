f3f3e507d4c82fa1662f4bfd98106833c4457692
Sergei Zenevich <zenevichsa@gmail.com>
2015-03-20 15:20:48 +0300

    Moved on 1.1

--- sample/src/main/java/com/softteco/toolset/sample/persons/PersonDaoBean.java
+++ sample/src/main/java/com/softteco/toolset/sample/persons/PersonDaoBean.java
@@ -7,9 +7,4 @@ import com.softteco.toolset.jpa.AbstractJpaDao;
  * @author serge
  */
 public class PersonDaoBean extends AbstractJpaDao<PersonEntity, Long> implements PersonDao {
-
-    @Override
-    protected Class<PersonEntity> getEntityClass() {
-        return PersonEntity.class;
-    }
 }

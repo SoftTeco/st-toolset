e16c652c691824f51a015405079c59f5b444e7f1
Andrei Belan <a.belan@softteco.com>
2015-06-26 16:36:07 +0300

    Add example of properties file. Set up Jetty server to run the example project, using of jndi for datasource.

---
+++ sample/src/main/java/com/softteco/toolset/sample/util/ApplicationUtils.java
@@ -0,0 +1,22 @@
+package com.softteco.toolset.sample.util;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ *
+ */
+public class ApplicationUtils {
+
+    private static String appPath;
+
+    public static String getAppPath() {
+        if (appPath == null) {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            URL url = (classLoader != null) ? classLoader.getResource("/") : null;
+            appPath = (url != null) ? url.getPath() : File.separator;
+        }
+        return appPath;
+    }
+
+}

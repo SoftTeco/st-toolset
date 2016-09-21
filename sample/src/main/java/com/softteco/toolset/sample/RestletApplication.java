f632df89d60ee5dbce4ab2859489783a540c5896
dkuhta <kuhta.denis@gmail.com>
2016-09-21 11:13:36 +0300

    Added swagger integration for api docs generation

--- sample/src/main/java/com/softteco/toolset/sample/RestletApplication.java
+++ sample/src/main/java/com/softteco/toolset/sample/RestletApplication.java
@@ -4,6 +4,8 @@ import com.softteco.toolset.restlet.AbstractRestletApplication;
 import com.softteco.toolset.restlet.AbstractStatusService;
 import com.softteco.toolset.sample.persons.PersonsResource;
 import org.restlet.routing.Router;
+import org.restlet.ext.swagger.Swagger2SpecificationRestlet;
+import org.restlet.ext.swagger.SwaggerSpecificationRestlet;
 
 /**
  *
@@ -19,5 +21,24 @@ public class RestletApplication extends AbstractRestletApplication {
     @Override
     protected void createInboundRoot(final Router router) {
         router.attach("/persons", PersonsResource.class);
+
+        /*
+          ../api/swagger.json
+          ../api/api-docs
+        */
+        attachSwaggerSpecification1(router);
+        attachSwaggerSpecification2(router);
+    }
+
+    private void attachSwaggerSpecification1(Router router) {
+        SwaggerSpecificationRestlet swaggerSpecificationRestlet = new SwaggerSpecificationRestlet(this);
+        swaggerSpecificationRestlet.setBasePath("http://myapp.com/api/");
+        swaggerSpecificationRestlet.attach(router);
+    }
+
+    private void attachSwaggerSpecification2(Router router) {
+        Swagger2SpecificationRestlet restlet = new Swagger2SpecificationRestlet(this);
+        restlet.setBasePath("http://myapp.com/api/");
+        restlet.attach(router);
     }
 }

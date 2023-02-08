83615d840d4757fa68997b9d14e57630d6b19679
Serge Zenevich <serge@softteco.com>
2023-02-08 13:25:54 +0300

    Added sales kpi.

--- toolset/src/main/java/com/softteco/toolset/dto/BooleanDto.java
+++ toolset/src/main/java/com/softteco/toolset/dto/BooleanDto.java
@@ -1,11 +1,13 @@
 package com.softteco.toolset.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 
 /**
  *
  * @author sergeizenevich
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BooleanDto implements Serializable {
 
     public Boolean result;

f734c6e1e6f57336fb37740ac14cf768981a9fc5
fedosenk0 <e.fedosenko@softteco.com>
2024-06-24 10:06:27 +0300

    Revert "SN-691: Add to field into AbstractEmailBuilder."
    This reverts commit 5e5d7425de373c8e165cbf5261700835929ef1b0.

--- toolset/src/main/java/com/softteco/toolset/mail/AbstractEmailBuilder.java
+++ toolset/src/main/java/com/softteco/toolset/mail/AbstractEmailBuilder.java
@@ -3,14 +3,11 @@ package com.softteco.toolset.mail;
 import java.io.Serializable;
 
 public abstract class AbstractEmailBuilder implements EmailBuilder {
-
-    private String to;
-
     @Override
     public EmailDto build() {
         final EmailDto dto = new EmailDto();
         dto.externalId = getExternalId();
-        dto.to = to == null ? getTo() : to;
+        dto.to = getTo();
         dto.ccs = getCcs();
         dto.subject = getSubject();
         dto.html = getHtml();
@@ -26,8 +23,4 @@ public abstract class AbstractEmailBuilder implements EmailBuilder {
     protected abstract String[] getCcs();
 
     protected abstract String getTo();
-
-    public void setTo(final String to) {
-        this.to = to;
-    }
 }

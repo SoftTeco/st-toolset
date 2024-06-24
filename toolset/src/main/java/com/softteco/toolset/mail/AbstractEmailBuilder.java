7dbccd81c73a9be2539596eeed07ac4ac60888f2
fedosenk0 <e.fedosenko@softteco.com>
2024-06-24 10:19:02 +0300

    SN-691: Add to field into AbstractEmailBuilder.

--- toolset/src/main/java/com/softteco/toolset/mail/AbstractEmailBuilder.java
+++ toolset/src/main/java/com/softteco/toolset/mail/AbstractEmailBuilder.java
@@ -3,11 +3,14 @@ package com.softteco.toolset.mail;
 import java.io.Serializable;
 
 public abstract class AbstractEmailBuilder implements EmailBuilder {
+
+    private String to;
+
     @Override
     public EmailDto build() {
         final EmailDto dto = new EmailDto();
         dto.externalId = getExternalId();
-        dto.to = getTo();
+        dto.to = to == null ? getTo() : to;
         dto.ccs = getCcs();
         dto.subject = getSubject();
         dto.html = getHtml();
@@ -23,4 +26,8 @@ public abstract class AbstractEmailBuilder implements EmailBuilder {
     protected abstract String[] getCcs();
 
     protected abstract String getTo();
+
+    public void setTo(final String to) {
+        this.to = to;
+    }
 }
